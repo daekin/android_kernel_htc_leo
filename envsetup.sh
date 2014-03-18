6311b050524bd8af2ff94868a5b1e9a61afd46e2
Miroslav Kallo <miroslav@kallo.sk>
2014-03-18 23:08:14 +0100

    Initial configuration for Sense 4.1  - added and enabled bcm4329 driver  - updated htcleo_defconfig  - added alsa driver  - fixed compilation with O3

--- envsetup.sh
+++ envsetup.sh
@@ -1,2 +1,2 @@
 export ARCH=arm
-export CROSS_COMPILE=~/android/toolchains/arm-cortex_a8-linux-gnueabi-linaro_4.8.3-2014.02/bin/arm-cortex_a8-linux-gnueabi-
+export CROSS_COMPILE=~/android/toolchains/arm-cortex_a8-linux-gnueabi-linaro_4.7.4-2014.01/bin/arm-cortex_a8-linux-gnueabi-
